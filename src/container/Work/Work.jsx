722f5eb4cd3dc24453c659400b8776b12328df74
Steven Olmos <eaodesigns@gmail.com>
2022-10-21 22:49:27 -0700

    Testimonials

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { AiFillEye, AiFillGithub } from "react-icons/ai";
 import { motion } from "framer-motion";
-
 import { AppWrap, MotionWrap } from "../../wrapper";
 import { urlFor, client } from "../../client";
 import "./Work.scss";
